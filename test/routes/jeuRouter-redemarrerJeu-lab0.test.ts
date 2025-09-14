a06c1429648de3110bdaf1cc59904959bf9883e5
Nicolas.Lm <nicolaslemorzellec@hotmail.com>
2025-09-14 18:05:52 -0400

    étape 8 terminé

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -7,11 +7,13 @@ import request from 'supertest';
 import { JeuDeDes } from '../../src/core/jeuDeDes';
 import { jeuRoutes } from "../../src/routes/jeuRouter";
 
+const testNom1 = 'test1';
+const testNom2 = 'test2';
 
 describe('GET /api/v1/jeu/redemarrerJeu', () => {
   beforeEach(async () => {
-    await request(app).post('/api/v1/jeu/demarrerJeu').send({ nom: "test1" });
-    await request(app).post('/api/v1/jeu/demarrerJeu').send({ nom: "test2" });
+    await request(app).post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+    await request(app).post('/api/v1/jeu/demarrerJeu').send({ nom: testNom2 });
   });
 
   it("devrait retourner un status 200 et une réponse JSON", async () => {
@@ -25,6 +27,12 @@ describe('GET /api/v1/jeu/redemarrerJeu', () => {
     const joueursJSON = jeuRoutes.controleurJeu.joueurs;
     const joueursArray = JSON.parse(joueursJSON);
     expect(joueursArray.length).toBe(0);
+  });
+
+  it("devrait retourner un status 404 après avoir redemarrerJeu", async () => {
+    await request(app).get('/api/v1/jeu/redemarrerJeu');
+    const response = await request(app).get('/api/v1/jeu/jouer/' + testNom1);
+    expect(response.status).toBe(404);
   });
 });
 
